e6e359e5b376fd93d9e88a4d17cd99d8c70825b7
Shivang Kher <69949038+Shivang-kher@users.noreply.github.com>
2023-04-14 22:33:04 +0530

    delete 1

--- ChargeNation/routes/Paymentroutes.js
+++
@@ -1,9 +0,0 @@
-const {createOrder} =  require("../controllers/paymentControllers");
-const express = require("express");
-const router = express.Router();
-
-router.get('/createorder',createOrder);
-// router.post('/payment/callback',paymentCallback);
-
-
-module.exports=router;
